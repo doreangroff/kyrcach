7a4f3ee87099de6d0b73545bfa0607baa01853c5
Zaphkie1ka <skyrim.skyrim.1001@gmail.com>
2024-03-11 14:28:35 +0700

    окно с таблицами в процессе

---
+++ kyrcach/Windows/Baza.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+
+namespace kyrcach.Windows;
+
+public partial class Baza : UserControl
+{
+    public Baza()
+    {
+        InitializeComponent();
+    }
+
+    private void OpenDriversBaza(object? sender, TappedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ kyrcach/Windows/MainWindow.axaml.cs
@@ -0,0 +1,57 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using kyrcach.Windows;
+
+namespace kyrcach;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Close(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void ButtonOnFocus(object? sender, PointerEventArgs e)
+    {
+        var but = (Button)sender;
+        string x = but.Name;
+        Strelki(x, s1, s2, s3);
+    }
+
+    private void Strelki(string x, TextBlock s1, TextBlock s2, TextBlock s3)
+    {
+        switch (x)
+        {
+           case "Base":
+               this.s1.Opacity = 1;
+               this.s2.Opacity = 0;
+               this.s3.Opacity = 0;
+               break;
+           case "Settings":
+               this.s2.Opacity = 1;
+               this.s1.Opacity = 0;
+               this.s3.Opacity = 0;
+               break;
+           case "Exit":
+               this.s3.Opacity = 1;
+               this.s2.Opacity = 0;
+               this.s1.Opacity = 0;
+               break;
+        }
+    }
+
+    private void BazaButton(object? sender, RoutedEventArgs e)
+    {
+        Panel.Children.Clear();
+        Baza baza = new Baza();
+        Panel.Children.Add(baza);
+    }
+}
